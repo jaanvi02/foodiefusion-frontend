abff9e21f949064f1204895a864ab92e926db869
Jaanvi Chaudhary <89679703+jaanvi02@users.noreply.github.com>
2023-09-30 19:04:39 +0530

    Update Signup.js

--- src/components/screen/Signup.js
+++ src/components/screen/Signup.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import React from 'react'
 import {Link,useNavigate} from "react-router-dom"
-const baseurl="https://mernapp1-961s.onrender.com/";
+const baseurl="https://mernapp1-961s.onrender.com";
 export default function Signup() {
   let navigate=useNavigate();
     const[credentials,setcredentials]=useState({name:"",Email:"",Password:"",location:""})
